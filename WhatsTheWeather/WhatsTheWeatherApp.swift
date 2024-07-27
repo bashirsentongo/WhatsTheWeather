e8434fb381b26c915256b0ad6476697b7db368b7
Bashir <bashir@fsbtech.com>
2024-07-27 06:52:54 +0100

    Created Project

---
+++ WhatsTheWeather/WhatsTheWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsTheWeatherApp.swift
+//  WhatsTheWeather
+//
+//  Created by Bashir Sentongo on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsTheWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
